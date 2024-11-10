4d08554cf10f97c14e73d4001db7cb524dd644b5
lgpaula <l.gdepaula@hotmail.com>
2024-11-10 15:30:44 +0100

    Removed apple from ci/cd. Added ifdef for windows/linux

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include "gameModes/SinglePlayer.hpp"
 #include "include/Game.hpp"
-#include "include/Snake.hpp"
+#include <thread>
+#include <chrono>
+
+#ifdef _WIN32
+#include <windows.h>
+#elif __linux__
 #include <X11/Xlib.h>
+#endif
 
 int main() {
+    int screenWidth = 0;
+    int screenHeight = 0;
 
+#ifdef _WIN32
+    screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    screenHeight = GetSystemMetrics(SM_CYSCREEN);
+#elif __linux__
     Display* display = XOpenDisplay(nullptr);
     if (display == nullptr) {
         std::cerr << "Cannot open display" << std::endl;
@@ -13,15 +25,17 @@ int main() {
     }
 
     Screen* screen = DefaultScreenOfDisplay(display);
-    int screenWidth = screen->width;
-    int screenHeight = screen->height;
+    screenWidth = screen->width;
+    screenHeight = screen->height;
+    XCloseDisplay(display);
+#endif
+
     std::cout << "screenHeight: " << screenHeight << " screenWidth: " << screenWidth << std::endl;
 
+//    std::unique_ptr<Game> game = std::make_unique<Game>(screenHeight, screenWidth);
     std::unique_ptr<Game> game = std::make_unique<Game>(1080, 1920);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-    XCloseDisplay(display);
 
     return 0;
 }
